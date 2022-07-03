08302cf1a9c07aea0d648728438ff454ca49eb10
wanguiwaweru <wanguiwawerub@gmail.com>
2022-07-03 13:06:46 +0300

    fetching data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect,useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/albums/1`)
+            const newData = await response.json()
+            setData(newData)
+        };
+
+        fetchData();
+    }, [])
+
+    if (data) {
+        console.log(data)
+        return <div className='App'>{data.title}</div>;
+    } else {
+        return null;
+    }
+    
 }
 
 export default App;
+
+
+
+
